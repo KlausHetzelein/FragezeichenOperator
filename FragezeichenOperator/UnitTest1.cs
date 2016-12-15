574b3a91a1514ec51f367338da21bd00d0853479
Klaus Hetzelein <klaus.hetzelein@gmx.de>
2016-12-15 14:06:13 +0100

    Add project files.

---
+++ FragezeichenOperator/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Principal;
+
+namespace FragezeichenOperator
+{
+    [TestClass]
+    public class QuestionmarkOperatorTests
+    {
+        [TestMethod]
+        public void UseQuestionmarkOperatorNotAtAll()
+        {
+            string name = GetNameTheClassicalWay(WindowsIdentity.GetCurrent());
+
+            Assert.IsNotNull(name);
+        }
+
+        [TestMethod]
+        public void UseQuestionmarkOperatorSingleOne()
+        {
+            string name = GetNameUsingQmOperator(WindowsIdentity.GetCurrent());
+
+            Assert.IsNotNull(name);
+        }
+
+        [TestMethod]
+        public void UseQuestionmarkOperatorSingleOneWithNullReference()
+        {
+            string name = GetNameUsingQmOperator(null);
+
+            Assert.IsNotNull(name);
+        }
+
+        [TestMethod]
+        public void UseExtensionMethodWithCorrectReference()
+        {
+            string name = WindowsIdentity.GetCurrent().SafelyGetName();
+
+            Assert.IsNotNull(name);
+        }
+
+        [TestMethod]
+        public void UseExtensionMethodWithNullReference()
+        {
+            string name = ((WindowsIdentity)null).SafelyGetName();
+
+            Assert.IsNotNull(name);
+        }
+
+        private string GetNameTheClassicalWay(WindowsIdentity windowsIdentity)
+        {
+            // the classic way
+            if (null != windowsIdentity)
+            {
+                if (null != windowsIdentity.User)
+                {
+                    return windowsIdentity.User.ToString();
+                }
+            }
+
+            return null;
+        }
+        private string GetNameUsingQmOperator(WindowsIdentity windowsIdentity)
+        {
+            return windowsIdentity?.Name?.ToString() ?? string.Empty;
+        }
+    }
+}
---
+++ FragezeichenOperator/WindowsIdentityExensions.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Security.Principal;
+
+static internal class WindowsIdentityExensions
+{
+    static internal string SafelyGetName(this WindowsIdentity windowsIdentity) 
+        => windowsIdentity?.Name?.ToString() ?? string.Empty;
+}
